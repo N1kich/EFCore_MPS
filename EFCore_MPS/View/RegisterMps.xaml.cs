450d237a9dc0fa72a0ab71f8818bf33b2b84de7f
ybrbx <funn1k.pro@gmail.com>
2023-06-17 18:35:31 +0300

    xaml markup for RegisterMps page

---
+++ EFCore_MPS/View/RegisterMps.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EFCore_MPS.View
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterMps.xaml
+    /// </summary>
+    public partial class RegisterMps : Page
+    {
+        public RegisterMps()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EFCore_MPS/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using EFCore_MPS.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_MPS.ViewModel
+{
+    class MainViewModel:ObservableObject
+    {
+        public MainViewModel() { }
+
+    }
+}

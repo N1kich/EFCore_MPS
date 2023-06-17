750c437b95ea3e27a5bfacc9f6864ed326e34c3a
ybrbx <funn1k.pro@gmail.com>
2023-06-17 21:45:16 +0300

    Navigation to RegisterMps page

--- EFCore_MPS/ViewModel/MainViewModel.cs
+++ EFCore_MPS/ViewModel/MainViewModel.cs
@@ -1,15 +1,33 @@
 ﻿using EFCore_MPS.Core;
+using EFCore_MPS.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace EFCore_MPS.ViewModel
 {
     class MainViewModel:ObservableObject
     {
-        public MainViewModel() { }
+        public RelayCommand ContentNavigationCommand { get; set; }
+        public Page ContentPage { get; set; }
+
+        private Uri _contentUri;
+        public Uri ContentUri { get { return _contentUri; } set { _contentUri = value; OnPropertyChanged(); } }
+        public MainViewModel() 
+        {
+            ContentNavigationCommand = new RelayCommand(o =>
+            {
+               ContentPage = new RegisterMps();
+                ContentUri = new Uri("/View/RegisterMps.xaml", UriKind.Relative);
+            });
+
+            
+        }
+
+        
 
     }
 }

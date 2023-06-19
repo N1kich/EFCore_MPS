6de8a793d41c1ede0b71ce312df8241aea0c0afd
ybrbx <funn1k.pro@gmail.com>
2023-06-19 15:36:07 +0300

    Added dialog window for creating new mps record

---
+++ EFCore_MPS/ViewModel/CreateMpsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EFCore_MPS.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_MPS.ViewModel
+{
+    class CreateMpsViewModel: ObservableObject
+    {
+        CreateMpsViewModel() 
+        {
+
+        }
+    }
+}

af5431e3f3bad7a15aa980671f76ca1ef452dfbe
ybrbx <funn1k.pro@gmail.com>
2023-06-21 19:00:44 +0300

    Added EFCore ApplicationContext

---
+++ EFCore_MPS/Core/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_MPS.Core
+{
+    class ApplicationDbContext: DbContext
+    {
+        private const string _connectionString = @"Data Source=DESKTOP-Q7UDDS1\SQLEXPRESS;Initial Catalog=mps;Integrated Security=True";
+
+        ApplicationDbContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
